d8e3a8a92422b6597678ceeb29ffa7a7a43501e8
Arthur Guilherme <161514706+ArthurGui004@users.noreply.github.com>
2025-02-27 11:46:00 -0300

    Alterando o Backend

--- Backend_Teste/main.py
+++ Backend_Teste/main.py
@@ -1,30 +1,40 @@
 from fastapi import FastAPI, Form, Request
 from pydantic import EmailStr
 from fastapi.templating import Jinja2Templates
+import uvicorn
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+
+database = []
 
 @app.get("/")
 def root():
     return {'message': 'Mensagem recebida com sucesso!'}
 
 @app.post("/registro")
-def exibir_dados(request: Request, name:str = Form(), email:EmailStr = Form(), password:str = Form()):
+def create_user(request: Request, user:str = Form(), email:EmailStr = Form(), password:str = Form()):
+    user_id = len(database) + 1
+    database.append([user_id, user, email, password])
+
     return templates.TemplateResponse("registro.html", {
         "request": request,
         "message": 'Dados Recebidos',
-        "name": name,
+        "id": user_id,
+        "user": user,
         "email": email,
         "password": password
     })
 
 @app.post("/login")
-def exibir_dados(request: Request, email:EmailStr = Form(), password:str = Form()):
-    return templates.TemplateResponse("registro.html", {
+def login_user(request: Request, email:EmailStr = Form(), password:str = Form()):
+    return templates.TemplateResponse("login.html", {
         "request": request,
         "message": 'Login efetuado!',
         "email": email,
         "password": password
     })
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
